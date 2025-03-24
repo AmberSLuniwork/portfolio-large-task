35223779c4498caa4f9d1bff61e75bca9432b271
Amber <25276620@edgehill.ac.uk>
2025-03-24 10:20:51 +0000

    A bit of everythin here. Header and footer templates have been updated completely, some modifications to the templates file and style updates for a mobile menu.

--- assets/script/templates.js
+++ assets/script/templates.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header"); // Selects the existing <nav> element
     if (header) {
         header.innerHTML = `
-        <p class="logo">Company Name</p>
+        <a class="logo" href="index.html">Company Name</p>
         <nav>
             <button class="menu-toggle" id="hamburger">&#9776;</button>
             <ul class="nav-list">
@@ -38,25 +38,31 @@ document.addEventListener("DOMContentLoaded", function () {
     if (footer) {
         footer.innerHTML = `
         <section class="links">
-            links should be put here!
+            <ul class="footer-nav">
+                <li>
+                    <a href=index.html>Home</a>
+                </li>
+                <li>
+                    <a href=about.html>About Us</a>
+                </li>
+                <li>
+                    <a href=services.html>Services</a>
+                </li>
+                <li>
+                    <a href=portfolio.html>Portfolio</a>
+                </li>
+                <li>
+                    <a href=our-process.html>Our Process</a>
+                </li>
+                <li>
+                    <a href=blog.html>Our Insights</a>
+                </li>
+                <li>
+                    <a href=contact.html>Contact Us</a>
+                </li>
+            </ul>
         </section>
-        <section class="formcontainer">
-            Send us a message.
-            <form id="contactform">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
-            
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-            
-                <label for="subject">Subject:</label>
-                <input type="text" id="subject" name="subject" required>
-            
-                <label for="message">Message:</label>
-                <textarea id="message" name="message" required></textarea>
-                <button type="submit">Send</button>
-            </form>  
-        </section>
+        <section class="awards"></section>
         `;
     }
 });
